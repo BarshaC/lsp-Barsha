5499dadf919aa19e9fb07baafef74d472e97a270
Barsha Chaudhary <64405568+BarshaC@users.noreply.github.com>
2022-09-23 15:30:28 -0400

    Assignment #1

---
+++ lsp-Barsha/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Barsha Chaudhary
+ * 
+ * @author barsha
+ *
+ *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
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+	}
+
+}
